62f4b693ef9a566c58044f21cd55303337e69ca3
agustrinaldokurniawan <agustkurniawan010899@gmail.com>
2021-08-27 08:27:07 +0700

    invoice

--- index.js
+++ index.js
@@ -1,39 +1,54 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-require('dotenv').config()
+require("dotenv").config();
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 
 try {
-const connectMongoose = mongoose.connect("mongodb://localhost:27017/first", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).catch(err=>{
-  throw new Error(err)
-})
+  const connectMongoose = mongoose
+    .connect("mongodb://localhost:27017/first", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
 
-if(connectMongoose) console.log("Mongoose Connected")
+  if (connectMongoose) console.log("Mongoose Connected");
 
-const app = express();
+  const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
 
-const userRoutes = require("./routes/user");
-const mediaRoutes = require("./routes/media");
-const blogRoutes = require("./routes/blog");
-const addressRoutes = require("./routes/address");
-const paymentRoutes = require("./routes/payment")
+  const {
+    route: userRoutes,
+    openapiSpecification: userSpec,
+  } = require("./routes/user");
 
-app.use("/users", userRoutes);
-app.use("/media", mediaRoutes);
-app.use("/blog", blogRoutes);
-app.use("/address", addressRoutes);
-app.use("/blog", blogRoutes);
-app.use("/payment", paymentRoutes);
+  const mediaRoutes = require("./routes/media");
+  const {
+    route: blogRoutes,
+    openapiSpecification: blogSpec,
+  } = require("./routes/blog");
 
-const server =app.listen(3000)
-if(server) console.log("Server running is success")
+  const addressRoutes = require("./routes/address");
+  const paymentRoutes = require("./routes/payment");
 
+  app.use("/users", userRoutes);
+  app.use("/media", mediaRoutes);
+  app.use("/blog", blogRoutes);
+  app.use("/address", addressRoutes);
+  app.use("/blog", blogRoutes);
+  app.use("/payment", paymentRoutes);
+
+  app.use("/api-docs/user", swaggerUi.serve, swaggerUi.setup(userSpec));
+  app.use("/api-docs/blog", swaggerUi.serve, swaggerUi.setup(blogSpec));
+
+  const server = app.listen(3000);
+  if (server) console.log("Server running is success");
 } catch (error) {
-  console.log(error)
+  console.log(error);
 }
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const UserController = require("../controllers/user");
 const route = express.Router();
+const swaggerJsdoc = require("swagger-jsdoc");
+
+const option = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "User",
+      version: "1.0.0",
+    },
+  },
+  apis: ["./routes/user.js"],
+};
+
+const openapiSpecification = swaggerJsdoc(option);
+
+/**
+ * @openapi
+ * /login:
+ *   post:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 
 route
   .post("/signup", UserController.signup)
   .post("/login", UserController.signin);
 
-module.exports = route;
+module.exports = { route, openapiSpecification };
